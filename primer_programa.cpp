404b2a9dc0f9cb8123f6a7cf2e334e6184b84936
Samuel García <samueljose.garcia@iesdonana.org>
2023-12-20 17:55:03 +0100

    Carga inicial. Hola Mundo

---
+++ primer_programa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hola Mundo" << endl;
+
+    std::cout << "Prueba usando el operador de ámbito" << std::endl;
+
+    return 0;
+}
